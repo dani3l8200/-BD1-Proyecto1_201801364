2eb8f2c14949c1faf0e179d9d10d1a112fecddd0
Juan Daniel <dani3l25948@gmail.com>
2021-03-28 09:54:14 +0000

    Consultas 1 y 2 terminadas

---
+++ consulltas/consultas.sql
@@ -0,0 +1,71 @@
+/************************************************CONSULTA 1 **********************************************************************************/
+SELECT ele.nombre as "eleccion", ele.anio "año eleccion", p.nombre "pais",
+par.nombre "nombre_partido", par.partido "Partido", ROUND((SUM(cv.analfabetos+cv.alfabetos)/(SELECT SUM(SUM(cv.analfabetos+cv.alfabetos))
+FROM conteovotos cv
+INNER JOIN eleccionpartido elep ON cv.id_eleccion_partido = elep.id_eleccion_partido
+INNER JOIN eleccion ele on elep.id_eleccion = ele.id_eleccion
+group by ele.nombre 
+))*100,2) as "Total%" FROM conteovotos cv
+INNER JOIN eleccionpartido elep ON cv.id_eleccion_partido = elep.id_eleccion_partido
+INNER JOIN municipio m ON cv.id_municipio = m.id_municipio
+INNER JOIN departamento d ON m.id_departamento = d.id_departamento
+INNER JOIN region r ON d.id_region = r.id_region
+INNER JOIN pais p ON r.id_pais = p.id_pais
+INNER JOIN eleccion ele on elep.id_eleccion = ele.id_eleccion
+INNER JOIN partido par on elep.id_partido = par.id_partido
+group by ele.anio, ele.nombre, par.nombre, par.partido, p.nombre
+ORDER BY "Total%" desc;
+/******************************************************************************************************************************************************/
+/****************************************************** CONSULTA 2 ***********************************************************************************/
+SELECT p.nombre as pais, d.nombre as departamento, SUM(cv.analfabetos+cv.alfabetos) as TotalDepartamento,
+
+(SELECT sum(cv.alfabetos+cv.analfabetos) from conteovotos cv 
+INNER JOIN municipio mm ON cv.id_municipio = mm.id_municipio
+INNER JOIN sexo sex ON cv.id_sexo = sex.id_sexo
+INNER JOIN departamento dd ON mm.id_departamento = dd.id_departamento
+INNER JOIN region rr ON dd.id_region = rr.id_region
+INNER JOIN pais pp ON rr.id_pais = pp.id_pais
+where pp.nombre = p.nombre and 
+sex.nombre = 'mujeres'
+group by pp.nombre
+) as TotalPais,
+
+ROUND((SUM(cv.analfabetos+cv.alfabetos))
+/(SELECT sum(sum(cv.alfabetos+cv.analfabetos)) from conteovotos cv 
+INNER JOIN municipio mm ON cv.id_municipio = mm.id_municipio
+INNER JOIN sexo sex ON cv.id_sexo = sex.id_sexo
+INNER JOIN departamento dd ON mm.id_departamento = dd.id_departamento
+INNER JOIN region rr ON dd.id_region = rr.id_region
+INNER JOIN pais pp ON rr.id_pais = pp.id_pais
+where sex.nombre = 'mujeres'
+and pp.nombre = p.nombre
+group by pp.nombre),2) as PorcentajeDepartamentos,
+
+ROUND((SELECT sum(sum(cv.alfabetos+cv.analfabetos)) from conteovotos cv 
+INNER JOIN municipio mm ON cv.id_municipio = mm.id_municipio
+INNER JOIN sexo sex ON cv.id_sexo = sex.id_sexo
+INNER JOIN departamento dd ON mm.id_departamento = dd.id_departamento
+INNER JOIN region rr ON dd.id_region = rr.id_region
+INNER JOIN pais pp ON rr.id_pais = pp.id_pais
+where sex.nombre = 'mujeres'
+and pp.nombre = p.nombre
+group by pp.nombre)/(SELECT sum(sum(cv.alfabetos+cv.analfabetos)) from conteovotos cv 
+INNER JOIN municipio mm ON cv.id_municipio = mm.id_municipio
+INNER JOIN sexo sex ON cv.id_sexo = sex.id_sexo
+INNER JOIN departamento dd ON mm.id_departamento = dd.id_departamento
+INNER JOIN region rr ON dd.id_region = rr.id_region
+INNER JOIN pais pp ON rr.id_pais = pp.id_pais
+where sex.nombre = 'mujeres'
+group by pp.nombre),2) as PorcentajePais
+
+FROM conteovotos cv
+INNER JOIN sexo sex ON cv.id_sexo = sex.id_sexo
+INNER JOIN municipio m ON cv.id_municipio = m.id_municipio
+INNER JOIN departamento d ON m.id_departamento = d.id_departamento
+INNER JOIN region r ON d.id_region = r.id_region
+INNER JOIN pais p ON r.id_pais = p.id_pais
+where sex.nombre = 'mujeres'
+group by  p.nombre, d.nombre
+order by TotalPais desc;
+/******************************************************************************************************************************************************/
+/******************************************************************Consulta 3 *************************************************************************/
